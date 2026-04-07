09d4c670bf92fd67430642750a923dd34ff17512
hani7 <h.informatique7@gmail.com>
2026-04-07 10:47:30 +0100

    first

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,14 @@
 import os
 import sys
-
 from pathlib import Path
 
-# Add the project base directory to the sys.path
+# Add project root to sys.path
 cwd = Path(__file__).resolve().parent
 sys.path.insert(0, str(cwd))
 
-# Set the settings module (replace 'abo.settings' if needed)
+# Point to your settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abo.settings')
 
-# Get the WSGI application
+# Get Django application
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
