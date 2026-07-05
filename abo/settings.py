a90e676515183280b063551e5cb08620bab55909
hani7 <h.informatique7@gmail.com>
2026-07-05 19:28:13 +0100

    add sudan intern flight

--- abo/settings.py
+++ abo/settings.py
@@ -42,7 +42,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bv%0ox!y+6lcf3^tw-4hq^)vg3lq#ex$g$v+3()tm3#$v_uhcf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_ENV', 'development') == 'development'
+DEBUG = False
 
 # ALLOWED_HOSTS from env or default
 env_hosts = os.environ.get('ALLOWED_HOSTS', '')
